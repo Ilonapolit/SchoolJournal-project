5d8c7819ea53e0dc83cd466cdec2c3e4e6359b42
Ilonapolit <139139277+Ilonapolit@users.noreply.github.com>
2023-12-09 18:28:37 +0400

    WeeksSubjects-massive

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,33 @@
-import { CreateUser } from "./features/CreateUser"
-import {SApp,STable} from "./App.styled"
-import { TableHeader } from "./features/TableHeader"
-import { TableRow } from "./features/TableRow"
+const App = () => {
+  const subjects = ['math', 'english', 'georgian'];
+  const weeks = ['Week1', 'Week2', 'Week3'];
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 
-import {useContext} from "react"
-import { GlobalContext } from "./GlobalContext.jsx/GlobalContext"
-function App() {
-
-  const {users} =useContext(GlobalContext)
-
-  return (
-    <SApp>
-     <CreateUser/>
-     <STable>
-
-    <TableHeader/>
-    <tr>
-      <td>სახელი გვარი</td>
-      <td>-</td>
-      <td>-</td>
-      <td>100</td>
-      <td>30</td>
-    </tr>
-    <tr>
-      <td>ანი</td>
-      <td>30</td>
-      <td>50</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
-   {users.map((user)=> {
-    return <TableRow key={user.id} user={user} />
-   }
-
-   )}
- <TableRow/>
-  /* <tr>
-    <th>Students</th>
-    <th>Contact</th>
-
-
-import React from 'react'
-
-export function TableRow() {
-export function TableRow({user}) {
-  return (
-    <div>
-
-    </div>
-    <tr>
-      <td>{user.username}</td>
-      <td>-</td>
-      <td>-</td>
-      <td>100</td>
-      <td>30</td>
-    </tr>
-  )
-}
-
-export default App
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [selectedWeek, setSelectedWeek] = useState(weeks[0]);
+  const [students, setStudents] = useState([
+    { 
+      id: 1, 
+      name: 'John', 
+      surname: 'Doe', 
+      grades: {
+        math: {
+          Week1: [null, null, null, null, null],
+          Week2: [null, null, null, null, null],
+          Week3: [null, null, null, null, null]
+        },
+        english: {
+          Week1: [null, null, null, null, null],
+          Week2: [null, null, null, null, null],
+          Week3: [null, null, null, null, null]
+        },
+        georgian: {
+          Week1: [null, null, null, null, null],
+          Week2: [null, null, null, null, null],
+          Week3: [null, null, null, null, null]
+        }
+      } 
+    },
+  ]);
 
 
